2d7b7fe0267c745d78b9c44a18e0e4a4b5c8bd30
Alan Schmitz <40610122+alan-schmitz@users.noreply.github.com>
2025-08-12 20:34:14 -0300

    Aula 08 desafio 08

---
+++ Aula_8/aula08_atividade01.py
@@ -0,0 +1,17 @@
+##
+  # aula 08 - Atividade 01 - exercicio 01
+  # Autor  Alan 
+  # Data 12/08/2025
+##
+"""
+ função saudação
+   - imprime uma mensagem de boas vindas 
+"""
+
+def saudacao ():
+    print(f'\n\n\tOlá Usuario seja bem vindo!!\n\n')
+
+##
+# corpo do progama
+##
+saudacao()
